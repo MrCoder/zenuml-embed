bafada68891dc45378ead1e3dd74f308b902922d
zenuml <github.bot@zenuml.com>
2023-01-21 04:53:54 +0000

    Update from https://github.com/ZenUml/core/commit/2d6db10544a4b3a2051ed871ad7fddcb361e4db9

---
+++ assets/main-b8481dd6.js
@@ -0,0 +1,4 @@
+import{r as n,Z as m}from"./core-0eb15553.js";const d=n.child({name:"main"}),e=document.querySelector("pre.zenuml");var o;const c=((o=e==null?void 0:e.textContent)==null?void 0:o.trim())||` 
+// comment
+A
+A.method`,t=new m(e);console.log("set zenUML to window");window.zenUml=t;t.render(c,"theme-default").then(r=>{d.debug("render resolved",r)});window.parentLogger=n;
