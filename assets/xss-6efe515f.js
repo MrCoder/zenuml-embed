01d580583d388a0af77e670584f7cbcd04aa101d
zenuml <github.bot@zenuml.com>
2023-01-24 22:45:21 +0000

    Update from https://github.com/ZenUml/core/commit/32ae0c6c2debd9e40d910ba8c9bbcf75096af10c

--- assets/xss-6efe515f.js
+++
@@ -1,1 +0,0 @@
-import{r as e,Z as n}from"./core-0eb15553.js";const t=e.child({name:"main"}),m=document.querySelector("#diagram"),d='"><img src=x onerror=alert(1)>',r=new n(m);window.zenUml=r;r.render(d,"theme-default").then(o=>{t.debug("render resolved",o)});window.parentLogger=e;
