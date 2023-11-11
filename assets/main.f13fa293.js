5ea8d139408267598eb623598959cf7ec6adb042
zenuml <github.bot@zenuml.com>
2023-11-11 06:02:22 +0000

    Update from https://github.com/mermaid-js/zenuml-core/commit/42615d831625b5b22b045b59ca3c27cdc6ba9e59

--- assets/main.f13fa293.js
+++
@@ -1,4 +0,0 @@
-import{r as n,Z as m}from"./core.3bf32fc3.js";const d=n.child({name:"main"}),e=document.querySelector("pre.zenuml");var o;const c=((o=e==null?void 0:e.textContent)==null?void 0:o.trim())||`
-// comment
-A
-A.method`,t=new m(e);console.log("set zenUML to window");window.zenUml=t;t.render(c,{theme:"theme-default"}).then(r=>{d.debug("render resolved",r)});window.parentLogger=n;
